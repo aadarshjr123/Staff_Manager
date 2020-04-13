aa2056f445b5d64b95904361284f636e12af2c9d
Aadarsh Jr <55702370+aadarshjr123@users.noreply.github.com>
2020-04-13 17:08:09 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,14 +10,14 @@ import LoginForm from './components/LoginForm';
 class App extends Component {
   async componentDidMount() {
     var firebaseConfig = {
-      apiKey: 'AIzaSyAswSbxZgaIOYrMWK0w7XJwq2jei8NGPEo',
-      authDomain: 'employeesdetailsapp.firebaseapp.com',
-      databaseURL: 'https://employeesdetailsapp.firebaseio.com',
-      projectId: 'employeesdetailsapp',
-      storageBucket: 'employeesdetailsapp.appspot.com',
-      messagingSenderId: '827708131336',
-      appId: '1:827708131336:web:2d5eb5537a389d39baa9bd',
-      measurementId: 'G-5DPVHLGG2J',
+      apiKey:apiKey ,
+      authDomain: authDomain,
+      databaseURL: databaseURL,
+      projectId: projectId,
+      storageBucket: storageBucket,
+      messagingSenderId: messagingSenderId,
+      appId: appId,
+      measurementId: measurementId,
     };
     // Initialize Firebase
     firebase.initializeApp(firebaseConfig);
